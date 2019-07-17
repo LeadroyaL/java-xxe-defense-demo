13c15f6ec41204028f947fde824b21de065af3fe
LeadroyaL <243850435@qq.com>
2019-07-17 21:34:33 +0800

    增加更细的SAXParserFactory防御测试。

--- src/main/java/com/leadroyal/xxe/sample/SAXParserFactorySample.java
+++ src/main/java/com/leadroyal/xxe/sample/SAXParserFactorySample.java
@@ -12,10 +12,10 @@ import java.io.IOException;
 public class SAXParserFactorySample {
     public void safe() throws IOException, SAXException, ParserConfigurationException {
         SAXParserFactory spf = SAXParserFactory.newInstance();
-        spf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-        spf.setFeature("http://xml.org/sax/features/external-general-entities", false);
-        spf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
-        spf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        spf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true); // 开启可挡回显xxe和blink-xxe
+        spf.setFeature("http://xml.org/sax/features/external-general-entities", false); // 可能挡回显xxe，未测试，因为在这个case里，Handler完全是自己写
+        spf.setFeature("http://xml.org/sax/features/external-parameter-entities", false); // 开启可挡blink-xxe
+        spf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false); // 无效
         SAXParser parser = spf.newSAXParser();
         parser.parse(ResourceUtils.getPoc1(), (HandlerBase) null);
     }
