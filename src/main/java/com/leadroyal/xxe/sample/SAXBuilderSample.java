394295402d06bd7cd2d2412b4039b12d8225fd77
LeadroyaL <243850435@qq.com>
2019-07-17 21:00:07 +0800

    增加更细的SAXBuilder的防御测试

--- src/main/java/com/leadroyal/xxe/sample/SAXBuilderSample.java
+++ src/main/java/com/leadroyal/xxe/sample/SAXBuilderSample.java
@@ -10,14 +10,16 @@ import java.io.IOException;
 public class SAXBuilderSample {
     public void safe() throws JDOMException, IOException {
         SAXBuilder builder = new SAXBuilder();
-        builder.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-        builder.setFeature("http://xml.org/sax/features/external-general-entities", false);
-        builder.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        // 2019年7月17日20:24:45
+        // 测试环境8u172
+        builder.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true); // 开启可挡回显xxe和blink-xxe
+        builder.setFeature("http://xml.org/sax/features/external-general-entities", false); // 无效
+        builder.setFeature("http://xml.org/sax/features/external-parameter-entities", false); // 开启可挡blink-xxe
         Document doc = builder.build(ResourceUtils.getPoc1());
     }
 
-    public void safe2() throws JDOMException, IOException {
-        SAXBuilder builder = new SAXBuilder(true);
+    public void unsafe2() throws JDOMException, IOException {
+        SAXBuilder builder = new SAXBuilder(true); // 对blind-xxe无效，好像可以挡回显xxe，
         Document doc = builder.build(ResourceUtils.getPoc1());
     }
 
@@ -33,7 +35,7 @@ public class SAXBuilderSample {
 
     public static void test2() {
         try {
-            new SAXBuilderSample().safe2();
+            new SAXBuilderSample().unsafe2();
         } catch (JDOMException e) {
             e.printStackTrace();
         } catch (IOException e) {
