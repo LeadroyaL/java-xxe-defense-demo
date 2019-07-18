c4596a4b8394a4acffa63b1407c981851948ee31
LeadroyaL <243850435@qq.com>
2019-07-18 10:38:00 +0800

    增加更细的XMLInputFactory防御测试。

--- src/main/java/com/leadroyal/xxe/sample/XMLInputFactorySample.java
+++ src/main/java/com/leadroyal/xxe/sample/XMLInputFactorySample.java
@@ -3,15 +3,16 @@ package com.leadroyal.xxe.sample;
 import com.leadroyal.xxe.ResourceUtils;
 
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
 public class XMLInputFactorySample {
     public void safe() throws XMLStreamException {
         XMLInputFactory xmlInputFactory = XMLInputFactory.newFactory();
-        xmlInputFactory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
-        xmlInputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
-        XMLStreamReader xsr = xmlInputFactory.createXMLStreamReader(ResourceUtils.getPoc1());
+        xmlInputFactory.setProperty(XMLInputFactory.SUPPORT_DTD, false); // 开启可挡回显xxe和blink-xxe
+        xmlInputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false); // 开启可挡回显xxe和blink-xxe
+        XMLStreamReader reader = xmlInputFactory.createXMLStreamReader(ResourceUtils.getPoc1());
     }
     public static void test(){
         try {
